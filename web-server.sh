2a116531a19a2ae37afd7f4a270e703aa9c6164e
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-12-05 15:31:02 -0300

    apt get install curl in mongo function

--- web-server.sh
+++ web-server.sh
@@ -47,6 +47,7 @@ backend(){
 
 # In case of mongo
 mongoFunction(){
+    sudo apt install curl
     sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo apt install docker docker-compose -y
