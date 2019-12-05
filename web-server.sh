d1ffe578c7c54686a03ca5235aa1795a89b99907
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-12-05 14:16:44 -0300

    fix: some bugs with yarn

--- web-server.sh
+++ web-server.sh
@@ -71,7 +71,6 @@ yarnFunction(){
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt install yarn -y
-    nodeFunction
 }
 
 gitFunction(){
